1c1e2681391e5446dceb859b36487510626ea18d
aaronxie <aaronxie00000@gmail.com>
2025-03-14 01:57:50 -0700

    ground contact

--- dynamic_body_sim/actuated_pendulum/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Actuated Pendulum Simulation Package
-
-This package provides tools for simulating and analyzing an actuated pendulum using MuJoCo.
-"""
-
-from dynamic_body_sim.actuated_pendulum.simulate import run_simulation
-
-__all__ = ['run_simulation'] 
